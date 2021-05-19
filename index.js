ad88667ccdbe9f32c472a3eb133795808c52a040
Arnav Gupta <59139918+AshuGuptaGamer@users.noreply.github.com>
2021-05-19 21:02:52 +0530

    Fixes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const consoleManager = require('./managers/commandManager');
+const commandManager = require('./managers/commandManager');
 
 module.exports.init = options => {
     if (options && options.removeHelpCommand) return commandManager;
-    consoleManager.addCommand({
+    commandManager.addCommand({
         command: 'help',
         handler: async () => {
             let description = '\n';
-            consoleManager.allCommands.forEach(commandObj => description += `${commandObj.command} : ${commandObj.description}\n`);
+            commandManager.allCommands.forEach(commandObj => description += `${commandObj.command} : ${commandObj.description}\n`);
             console.log('==============Commands==============');
             console.log(description);
             console.log('====================================');
